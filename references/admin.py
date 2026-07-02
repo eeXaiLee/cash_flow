8dab8030a88714ce509e3f7f4f8bdfc4eb78a5e0
Evgeniy Dimitriev (work) <eexailee@gmail.com>
2026-07-02 15:39:08 +0500

    Feat: register all models in admin

--- references/admin.py
+++ references/admin.py
@@ -1,1 +1,29 @@
 from django.contrib import admin
+
+from .models import Category, OperationType, Status, Subcategory
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(OperationType)
+class OperationTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'operation_type')
+    list_filter = ('operation_type',)
+    search_fields = ('name',)
+
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category')
+    list_filter = ('category',)
+    search_fields = ('name',)
--- transactions/admin.py
+++ transactions/admin.py
@@ -1,1 +1,32 @@
 from django.contrib import admin
+
+from .models import Operation
+
+
+@admin.register(Operation)
+class OperationAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'date',
+        'status',
+        'operation_type',
+        'category',
+        'subcategory',
+        'amount',
+        'comment'
+    )
+    list_filter = (
+        'status',
+        'operation_type',
+        'category',
+        'subcategory',
+        'date'
+    )
+    search_fields = ('comment',)
+    date_hierarchy = 'date'
+    list_select_related = (
+        'status',
+        'operation_type',
+        'category',
+        'subcategory'
+    )

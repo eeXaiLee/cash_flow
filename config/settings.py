4f51e7d01422326692195ea3ade0223ecc03ce16
Evgeniy Dimitriev (work) <eexailee@gmail.com>
2026-07-02 13:17:50 +0500

    Chore: add references and transactions apps, configure DRF

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'django_filters',
+
+    'references',
+    'transactions',
 ]
 
 MIDDLEWARE = [
@@ -101,3 +107,10 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+}
